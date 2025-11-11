ad0ca44119a0b38912aac7939bee31b5ebaaab94
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-11-11 23:38:36 +0200

    Add base

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    base: "/",
     host: "::",
     port: 8080,
     allowedHosts: [
